fd2bc107858f5846e25b4157dfb15ac1a164dcb1
mixkio <mixkio@naver.com>
2020-11-11 02:10:07 +0900

    main

--- baekjoon/b2447.java
+++ baekjoon/b2447.java
@@ -1,42 +1,46 @@
-import java.util.Scanner;
-
-import javax.lang.model.util.ElementScanner14;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class b2447 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(bf.readLine());
         char[][] arr = new char[n][n];
-        printstar(arr, 0, 0, n);
+        printstar(arr, n, 0, 0);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(arr[i][j]);
+                bw.write(arr[i][j]);
             }
-            System.out.println();
+            bw.write("\n");
         }
-        sc.close();
+        bw.flush();
+        bw.close();
+        bf.close();
     }
 
-    static void printstar(char[][] arr, int x, int y, int size) {
-        if (size != 3) {
-            for (int i = x; i < x + size; i += size / 3) {
-                for (int j = y; j < y + size; j += size / 3) {
-                    if (i == size / 3 && j == size / 3) {
-                        for (int a = i; a < i + size / 3; a++)
-                            for (int b = i; b < i + size / 3; b++)
-                                arr[a][b] = ' ';
-                    } else {
-                        printstar(arr, i, j, size / 3);
+    static void printstar(char[][] arr, int size, int x, int y) {
+        for (int i = x; i < x + size; i += size / 3) {
+            for (int j = y; j < y + size; j += size / 3) {
+                if (i == x + size / 3 && j == y + size / 3) {
+                    for (int a = i; a < i + size / 3; a++) {
+                        for (int b = j; b < j + size / 3; b++) {
+                            arr[a][b] = ' ';
+                        }
                     }
-                }
-            }
-        } else {
-            for (int i = x; i < x + size; i++) {
-                for (int j = y; j < y + size; j++) {
-                    if (i == 1 && j == 1)
-                        arr[i][j] = ' ';
-                    else
-                        arr[i][j] = '*';
+                } else {
+                    if (size == 3) {
+                        for (int a = i; a < i + size / 3; a++) {
+                            for (int b = j; b < j + size / 3; b++) {
+                                arr[a][b] = '*';
+                            }
+                        }
+                    } else {
+                        printstar(arr, size / 3, i, j);
+                    }
                 }
             }
         }
