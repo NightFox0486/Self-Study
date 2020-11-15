c19567af182be9d403a00a5f649837ecbb08f0ac
mixkio <mixkio@naver.com>
2020-11-15 22:21:22 +0900

    main

--- baekjoon/b2798.java
+++ baekjoon/b2798.java
@@ -13,13 +13,31 @@ public class b2798 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-        int max = 0;
+        int min = M;
+        int[] min_idx = new int[3];
         st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         for (int i = 0; i < N; i++) {
-
+            for (int j = 0; j < N; j++) {
+                if (j == i)
+                    continue;
+                for (int k = 0; k < N; k++) {
+                    if (k == i || k == j || arr[i] + arr[j] + arr[k] > M)
+                        continue;
+                    if (min > M - (arr[i] + arr[j] + arr[k])) {
+                        min = M - (arr[i] + arr[j] + arr[k]);
+                        min_idx[0] = i;
+                        min_idx[1] = j;
+                        min_idx[2] = k;
+                    }
+                }
+            }
         }
+        bw.write(Integer.toString(arr[min_idx[0]] + arr[min_idx[1]] + arr[min_idx[2]]));
+        bw.flush();
+        bw.close();
+        bf.close();
     }
 }
