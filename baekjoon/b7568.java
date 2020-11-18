8871462ac337e66674daee6ca454ab575e9cab0f
mixkio <mixkio@naver.com>
2020-11-18 18:40:51 +0900

    main

---
+++ baekjoon/b7568.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class b7568 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int N = Integer.parseInt(bf.readLine());
+        int[][] arr = new int[N][3];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < N; i++) {
+
+        }
+        for (int i = 0; i < N; i++) {
+            bw.write(Integer.toString(arr[i][2]) + "\n");
+        }
+        bw.flush();
+        bw.close();
+        bf.close();
+    }
+}
