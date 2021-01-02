21c9cce7230d45875dd07c4aca9b8447f489ce3f
Raccoon <mixkio@naver.com>
2021-01-02 22:46:34 +0900

    main

--- python_study/b15649.py
+++ python_study/b15649.py
@@ -1,2 +1,7 @@
 import sys
+import math
 N, M = list(map(int, sys.stdin.readline().split()))
+for i in range(math.factorial(N)-math.factorial(N-M)):
+    for j in range(M):
+        print()
+    print()
