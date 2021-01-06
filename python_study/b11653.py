cf0c2daf84ecb9cbf003fa30edac61d2701786fb
Raccoon <mixkio@naver.com>
2021-01-06 11:59:47 +0900

    main

--- python_study/b11653.py
+++ python_study/b11653.py
@@ -1,17 +1,7 @@
 import sys
 
-
-def isPrime(N):
-    for i in range(2, N):
-        if N % i == 0:
-            return False
-    return True
-
-
 N = int(sys.stdin.readline())
 for i in range(2, N+1):
-    if isPrime(i) == False:
-        continue
     while N % i == 0:
         print(i)
         N /= i
