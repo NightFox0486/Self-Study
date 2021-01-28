782ab0c938e062396653af3271de28809c18fb6b
Raccoon <mixkio@naver.com>
2021-01-28 17:48:10 +0900

    main

--- Absolute_C++/2_4.cpp
+++ Absolute_C++/2_4.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int countDown;
 
     cout << "How many greetings do you want? ";
     cin >> countDown;
 
-    while (countDown > 0){
+    while (countDown > 0)
+    {
         cout << "Hello ";
         countDown = countDown - 1;
     }
