778ca17f151cd29168d8177ce1d86a64b0ace4e1
Raccoon <mixkio@naver.com>
2021-02-13 02:13:50 +0900

    main

---
+++ Absolute_C++/3_8.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+using namespace std;
+
+double estimateOfTotal(int minPeas, int maxPeas, int podCount);
+
+int main()
+{
+    int maxCount, minCount, podCount;
+    double averagePea, yield;
+    cout << "Enter minimum and maximum number of peas in a pod: ";
+    cin >> minCount >> maxCount;
+    cout << "Enter the number of pods: ";
+    cin >> podCount;
+    cout << "Enter the weight of an average pea (in ounces): ";
+    cin >> averagePea;
+    yield = estimateOfTotal(minCount, maxCount, podCount) * averagePea;
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(3);
+    cout << "Min number of peas per pod = " << minCount << endl
+         << "Max number of peas per pod = " << maxCount << endl
+         << "Pod count = " << podCount << endl
+         << "Average pea weight = "
+         << averagePea << " ounces" << endl
+         << "Estimated average yield = " << yield << " ounces"
+         << endl;
+    return 0;
+}
+
+double estimateOfTotal(int minPeas, int maxPeas, int podCount)
+{
+    double averagePea;
+    averagePea = (maxPeas + minPeas) / 2.0;
+    return (podCount * averagePea);
+}
