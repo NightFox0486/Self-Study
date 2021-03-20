51e10930af5e15ec79d2af7718d373a66e5dd421
mixkio <mixkio@naver.com>
2021-03-20 11:49:52 +0900

    main

---
+++ baekjoon/exam.java
@@ -0,0 +1,11 @@
+public class exam {
+    public static void main(String[] args) {
+        // solution(1, 2, 3);
+    }
+
+    static String solution(int day, int width, int blocks) {
+        String answer = "dd";
+
+        return answer;
+    }
+}
