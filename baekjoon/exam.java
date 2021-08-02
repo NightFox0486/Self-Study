8b3b1419112b90977d8256632c699814cd653acc
mixkio <mixkio@naver.com>
2021-08-03 01:06:51 +0900

    main

--- baekjoon/exam.java
+++ baekjoon/exam.java
@@ -1,11 +1,58 @@
+//import java.util.StringTokenizer;
+
 public class exam {
     public static void main(String[] args) {
-        // solution(1, 2, 3);
+        String[] table = { "SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++",
+                "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP",
+                "GAME C++ C# JAVASCRIPT C JAVA" };
+        String[] languages = { "JAVA", "JAVASCRIPT" };
+        int[] preference = { 7, 5 };
+        String answer = solution(table, languages, preference);
+        System.out.println(answer);
     }
 
-    static String solution(int day, int width, int blocks) {
-        String answer = "dd";
-
-        return answer;
+    private static String solution(String[] table, String[] languages, int[] preference) {
+        return null;
     }
+
+    // static String solution(String[] table, String[] languages, int[] preference)
+    // {
+    // String answer = "";
+    // StringTokenizer st;
+    // String[] sampletable = new String[5];
+    // int[] sumation = new int[5];
+    // String test = "";
+    // int max = 0;
+    // int selectcategorynum = 0;
+
+    // for (int i = 0; i < 5; i++) { // 각 계층별 검색;
+    // st = new StringTokenizer(table[i]);
+    // sampletable[i] = st.nextToken();
+    // for (int j = 0; j < 5; j++) {
+    // test = st.nextToken();
+    // for (int k = 0; k < languages.length; k++) { //
+    // if (test.equals(languages[k])) {
+    // sumation[i] += (5 - j) * preference[k]; // 매칭된 값을 저장;
+    // }
+    // }
+
+    // }
+    // }
+    // for (int i = 0; i < 5; i++) {
+    // if (max <= sumation[i]) {
+    // if (max == sumation[i]) {
+    // char[] ch1 = sampletable[selectcategorynum].toCharArray();
+    // char[] ch2 = sampletable[i].toCharArray();
+    // if (ch1[0] > ch2[0]) {
+    // selectcategorynum = i;
+    // }
+    // } else {
+    // selectcategorynum = i;
+    // }
+    // max = sumation[i];
+    // }
+    // }
+    // answer = sampletable[selectcategorynum];
+    // return answer;
+    // }
 }
---
+++ baekjoon/exam3.java
@@ -0,0 +1,121 @@
+import java.util.StringTokenizer;
+
+public class exam3 {
+    public static void main(String[] args) {
+        String program = "line";
+        String[] flag_rules = { "-s STRING", "-num NUMBER", "-e NULL", "-n ALIAS -num" };
+        String[] commands = { "line -n 100 -s hi -e", "line -n 100 -e -num 150" };
+        boolean[] answer = solution(program, flag_rules, commands);
+        System.out.println(answer);
+    }
+
+    static boolean[] solution(String program, String[] flag_rules, String[] commands) {
+        boolean[] answer = {};
+        boolean[] test = new boolean[commands.length];
+        StringTokenizer st;
+        String program_name;// 프로그램 이름 판별
+        String[] tags = new String[flag_rules.length];
+        String[] tags_type = new String[flag_rules.length];
+        String[] alias_tags = new String[flag_rules.length];
+        String commands_tags = "";
+        char[] commands_tags_seperator;
+        char[] commands_seperater;
+        String program_commands = "";
+        String commands_name = "";
+        boolean tags_flags = false;
+
+        for (int i = 0; i < flag_rules.length; i++) {
+            st = new StringTokenizer(flag_rules[i]);
+            tags[i] = st.nextToken();
+            tags_type[i] = st.nextToken();
+            if (tags_type[i].equals("ALIAS")) {
+                alias_tags[i] = st.nextToken();
+            }
+        }
+
+        for (int i = 0; i < commands.length; i++) {
+            test[i] = true;
+            st = new StringTokenizer(commands[i]);
+            program_name = st.nextToken();
+            if (!(program.equals(program_name)) || !(program.length() >= 1 && program.length() <= 10)) {
+                test[i] = false;///////// 수정
+                break;
+            }
+            do {
+                if (!tags_flags) {
+                    commands_tags = st.nextToken();
+                }
+                commands_tags_seperator = commands_tags.toCharArray();
+                if (tags_flags == true || commands_tags_seperator[0] == '-') {//////////////////////////// commands맞는
+                                                                              //////////////////////////// 태그를 찾아서 자료형을
+                                                                              //////////////////////////// 지정
+                    for (int j = 0; j < flag_rules.length; j++) {
+                        if (tags_flags) {
+                            break;
+                        }
+                        if (commands_tags.equals(tags[j]) || commands_tags.equals(alias_tags[j])) {
+                            commands_name = tags_type[j];
+                            if (commands_tags.equals(tags[j])) {
+                                alias_tags[j] = tags[j];
+                            }
+                            if (commands_tags.equals(alias_tags[j])) {
+                                tags[j] = alias_tags[j];
+                            }
+                            break;
+                        }
+                    }
+                    if (!(commands_name.equals("NULL"))) {
+                        if (!tags_flags) {
+                            program_commands = st.nextToken();
+                        }
+                        commands_seperater = program_commands.toCharArray();
+                        for (int j = 0; j < program_commands.length(); j++) {
+                            if (commands_name.equals("STRING")) {
+                                if (!((commands_seperater[j] >= 'a' && commands_seperater[j] <= 'z')
+                                        || (commands_seperater[j] >= 'A' && commands_seperater[j] <= 'Z'))) {
+                                    test[i] = false;
+                                    break;
+                                }
+                            }
+                            if (commands_name.equals("NUMBER")) {
+                                if (!(commands_seperater[j] >= '0' && commands_seperater[j] <= '9')) {
+                                    test[i] = false;
+                                    break;
+                                }
+                            }
+                            if (commands_name.equals("STRINGS")) {
+                                tags_flags = false;
+                                if (!((commands_seperater[j] >= 'a' && commands_seperater[j] <= 'z')
+                                        || (commands_seperater[j] >= 'A' && commands_seperater[j] <= 'Z'))) {
+                                    test[i] = false;
+                                    break;
+                                }
+                            }
+                            if (commands_name.equals("NUMBERS")) {
+                                tags_flags = false;
+                                if (!(commands_seperater[j] >= '0' && commands_seperater[j] <= '9')) {
+                                    test[i] = false;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    if (commands_name.equals("STRINGS") || commands_name.equals("NUMBERS")) {
+                        tags_flags = true;
+                    } else {
+                        test[i] = false;
+                        break;
+                    }
+                }
+            } while (st.hasMoreTokens());
+        }
+        if (test[1]) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        answer = test;
+        return answer;
+    }
+}
