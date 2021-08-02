5974efa750c480d63f865c93a0c48e98b4a6ec09
Raccoon <mixkio@naver.com>
2021-08-03 01:12:51 +0900

    main

--- baekjoon/exam.java
+++ baekjoon/exam.java
@@ -1,11 +1,12 @@
 public class exam {
     public static void main(String[] args) {
-        // solution(1, 2, 3);
+        System.out.println("2222");
+        solution(1, 2, 3);
     }
 
     static String solution(int day, int width, int blocks) {
         String answer = "dd";
-
+        System.out.println("sefsef");
         return answer;
     }
 }
