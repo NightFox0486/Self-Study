6500fe61623dac821106354dd67bfeae515e2db0
mixkio <mixkio@naver.com>
2021-09-07 01:35:07 +0900

    main

--- programmers/findPrime/Solution.java
+++ programmers/findPrime/Solution.java
@@ -1,6 +1,7 @@
 package findPrime;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 class Solution {
     public int solution(String numbers) {
@@ -12,12 +13,57 @@ class Solution {
             numint[i] = numchar[i] + 48;
         }
 
-        /// 모든 경우의수 탐색후 hash에 넣어서 중복 제거
+        searchAll(numchar, numset);
+        System.out.println(numset);
+        Iterator<Integer> itr = numset.iterator();
+        while (itr.hasNext()) {
+            if (isPrime(itr.next())) {
+                answer++;
+            }
+        }
 
         return answer;
     }
 
     boolean isPrime(int num) {
-        return false;
+        if (num == 0 || num == 1) {
+            return false;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    void searchAll(char[] numchar, HashSet<Integer> numSet) {
+        boolean[] flag = new boolean[numchar.length];
+        StringBuilder sbr = new StringBuilder();
+        for (int i = 0; i < numchar.length; i++) {
+            subroutine(numSet, sbr, flag, numchar, i + 1);
+            // System.out.println();
+        }
+    }
+
+    void subroutine(HashSet<Integer> numSet, StringBuilder sbr, boolean[] flag, char[] numchar, int length) {
+        if (length > 0) {
+            for (int j = 0; j < numchar.length; j++) {
+                if (flag[j]) {
+                    continue;
+                } else {
+                    flag[j] = true;
+                    sbr.append(numchar[j]);
+                    subroutine(numSet, sbr, flag, numchar, length - 1);
+                    flag[j] = false;
+                    if (length == 1) {
+                        numSet.add(Integer.valueOf(sbr.toString()));
+                        // System.out.print(sbr + "#");
+                        sbr.delete(sbr.length() - 1, sbr.length());
+                    }
+                }
+                if (length > 1)
+                    sbr.delete(sbr.length() - 1, sbr.length());
+            }
+        }
     }
 }
--- programmers/findPrime/main1.java
+++ programmers/findPrime/main1.java
@@ -3,7 +3,7 @@ package findPrime;
 public class main1 {
     public static void main(String[] args) {
         String numbers = "011";
-        Solution soluution = new Solution();
-        System.out.println(soluution.solution(numbers));
+        Solution solution = new Solution();
+        System.out.println(solution.solution(numbers));
     }
 }
