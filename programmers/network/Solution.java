c1780e8dbd47d5e265e0a55771e583e663660e9e
mixkio <mixkio@naver.com>
2021-09-09 15:23:33 +0900

    main

---
+++ programmers/network/Solution.java
@@ -0,0 +1,63 @@
+package network;
+
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] flag = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (flag[j]) {
+                    continue;
+                } else {
+                    if (i == j) {
+                        answer++;
+                        continue;
+                    } else {
+                        if (computers[i][j] == 1) {
+                            flag[j] = true;
+                            sub(j, n, computers, flag);
+                        }
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private void sub(int j, int n, int[][] computers, boolean[] flag) {
+        for (int i = 0; i < n; i++) {
+            if (flag[i]) {
+                continue;
+            } else {
+                if (i == j) {
+                    continue;
+                } else {
+                    if (computers[j][i] == 1) {
+                        flag[i] = true;
+                        sub(i, n, computers, flag);
+                    }
+                }
+            }
+        }
+    }
+
+    // class Node{
+    // static private Node head = null;
+    // static private int headcnt = 0;
+    // Node nextNode = null;
+    // int value = 0;
+
+    // Node(int value){
+    // this.value = value;
+    // }
+
+    // private void addNode(int value){
+    // Node node = new Node(value);
+    // if(head==null){
+    // head=node;
+
+    // }
+    // }
+    // }
+}
---
+++ programmers/network/main1.java
@@ -0,0 +1,10 @@
+package network;
+
+public class main1 {
+    public static void main(String[] args) {
+        int[][] computers = { { 1, 1, 0 }, { 1, 1, 1 }, { 0, 1, 1 } };
+        int n = computers.length;
+        Solution solution = new Solution();
+        System.out.println(solution.solution(n, computers));
+    }
+}
