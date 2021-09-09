f1612fdfa75b569bd24c9cda24c403b566d67e3b
mixkio <mixkio@naver.com>
2021-09-09 23:10:16 +0900

    main

---
+++ programmers/morespicier/Solution.java
@@ -0,0 +1,30 @@
+package morespicier;
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pQ = new PriorityQueue<>();
+        for (int i = 0; i < scoville.length; i++) {
+            pQ.add(scoville[i]);
+        }
+        while (pQ.peek() < K && pQ.peek() != null) {
+            Integer n1 = pQ.poll();
+            Integer n2 = pQ.poll();
+            if (n1 == null || n2 == null)
+                return -1;
+            pQ.add(mix(n1, n2));
+            answer++;
+        }
+        // System.out.println(pQ);
+        return answer;
+    }
+
+    private int mix(int n1, int n2) {
+        if (n1 > n2)
+            return (n1 * 2) + n2;
+        else
+            return (n2 * 2) + n1;
+    }
+}
---
+++ programmers/morespicier/main1.java
@@ -0,0 +1,10 @@
+package morespicier;
+
+public class main1 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] scoville = { 1, 2, 3, 9, 10, 12 };
+        int K = 10000;
+        System.out.println(solution.solution(scoville, K));
+    }
+}
