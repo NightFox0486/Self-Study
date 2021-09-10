8acce7b45b550eeda37004bbe10674e39fd3fce5
mixkio <mixkio@naver.com>
2021-09-11 01:36:22 +0900

    main

---
+++ programmers/AdInsertion/Solution.java
@@ -0,0 +1,64 @@
+package AdInsertion;
+
+class Solution {
+    public String solution(String play_time, String adv_time, String[] logs) {
+        String answer = "";
+        long[] all_time = new long[converttimetoInt(play_time)];
+        long max = 0;
+        long sum = 0;
+        for (int i = 0; i < logs.length; i++) {
+            String[] part_time = logs[i].split("-");
+            int start = converttimetoInt(part_time[0]);
+            int end = converttimetoInt(part_time[1]);
+            for (int j = start; j < end; j++) {
+                all_time[j]++;
+            }
+        }
+        int adv_timeInt = converttimetoInt(adv_time);
+        int pro_time = 0;
+        for (int i = 0; i < adv_timeInt; i++) {
+            sum += all_time[i];
+        }
+        max = sum;
+
+        for (int i = adv_timeInt; i < all_time.length; i++) {
+            sum += all_time[i] - all_time[i - adv_timeInt];
+            if (sum > max) {
+                max = sum;
+                pro_time = i - adv_timeInt + 1;
+            }
+        }
+        // int temp2 = all_time.length-adv_timeInt;
+        // for (int i = 0; i < all_time.length; i++) {
+        // sum = 0;
+        // if (i < temp2) {
+        // int temp = i+adv_timeInt;
+        // for (int j = i; j < temp; j++) {
+        // sum += all_time[j];
+        // }
+        // if (sum > max) {
+        // max = sum;
+        // pro_time = i;
+        // }
+        // }
+        // }
+
+        answer = convertInttotime(pro_time);
+        return answer;
+    }
+
+    private int converttimetoInt(String time) {
+        String[] h = time.split(":");
+        return Integer.parseInt(h[0]) * 3600 + Integer.parseInt(h[1]) * 60 + Integer.parseInt(h[2]);
+    }
+
+    private String convertInttotime(int time) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Integer.toString(time / 3600).format("%02d", time / 3600));
+        time -= (time / 3600) * 3600;
+        sb.append(":" + Integer.toString(time / 60).format("%02d", time / 60));
+        time -= (time / 60) * 60;
+        sb.append(":" + Integer.toString(time).format("%02d", time));
+        return sb.toString();
+    }
+}
---
+++ programmers/AdInsertion/main1.java
@@ -0,0 +1,12 @@
+package AdInsertion;
+
+public class main1 {
+    public static void main(String[] args) {
+        String play_time = "02:03:55";
+        String adv_time = "00:14:15";
+        String[] logs = { "01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29",
+                "01:37:44-02:02:30" };
+        Solution solution = new Solution();
+        System.out.println(solution.solution(play_time, adv_time, logs));
+    }
+}
