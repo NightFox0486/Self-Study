5f92954b90d6d18d84faa8fbc876428cd076720e
Raccoon <mixkio@naver.com>
2021-10-27 03:03:10 +0900

    main

--- programmers/truck_on_briedge/Solution.java
+++ programmers/truck_on_briedge/Solution.java
@@ -4,15 +4,32 @@ class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
         int current_weight = 0;
-        for (int i = 0; i < truck_weights.length;) {
-            if (current_weight < weight) {
-                current_weight += truck_weights[i];
-            } else {
-                answer += 1;
+        int i = 0;
+        int counter = 0;
+        int start = 0;
+        int end = 0;
+        boolean flag = true;
+        int[] cnt = new int[truck_weights.length];
+        while (flag) {
+            for (int j = start; j < end; j++) {
+                cnt[j]++;
+                if (cnt[j] == bridge_length) {
+                    current_weight -= truck_weights[j];
+                    start = j + 1;
+                    if (start == truck_weights.length) {
+                        flag = false;
+                        break;
+                    }
+                }
             }
-            answer += bridge_length - 1;
-            i++;
+            if (i < truck_weights.length && current_weight + truck_weights[i] < weight) {
+                current_weight += truck_weights[i];
+                i++;
+                end = i;
+            }
+            counter++;
         }
+        answer = counter;
 
         return answer;
     }
--- programmers/truck_on_briedge/main1.java
+++ programmers/truck_on_briedge/main1.java
@@ -2,9 +2,9 @@ package truck_on_briedge;
 
 public class main1 {
     public static void main(String[] args) {
-        int bridge_length = 3;
-        int weight = 10;
-        int[] truck_weights = { 7, 4, 5, 6 };
+        int bridge_length = 100;
+        int weight = 100;
+        int[] truck_weights = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
         Solution solution = new Solution();
         System.out.println(solution.solution(bridge_length, weight, truck_weights));
     }
