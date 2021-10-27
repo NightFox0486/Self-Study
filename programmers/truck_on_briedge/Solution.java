c1237a80db37624fb46b60db884be618ea3bcedf
mixkio <mixkio@naver.com>
2021-10-27 15:10:51 +0900

    main

--- programmers/truck_on_briedge/Solution.java
+++ programmers/truck_on_briedge/Solution.java
@@ -1,23 +1,25 @@
 package truck_on_briedge;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
+        Queue<Integer> bridege = new LinkedList<Integer>();
         int current_weight = 0;
-        for (int i = 0; i < truck_weights.length;) {
+        for (int i = 0; i < truck_weights[i]; i++) {
             if (current_weight < weight) {
                 current_weight += truck_weights[i];
-            } else {
-                answer += 1;
+                bridege.add(truck_weights[i]);
             }
-            answer += bridge_length - 1;
-            i++;
         }
 
         return answer;
     }
 }
 
-/*
- * abstract 현재 트럭과 뒤에 트럭들의 합이 weight보다 작으면 weight안넘을때 까지만 동시 진행 간격당 시간 1추가
- */
+// 한트럭당 무조건 breidge_length 만큼 시간 소비
+// weight 여유가 있으면 다음 시간에 바로 다음트럭 출발
+// weight가 허용하는한 최대 동시 진행수는 breidge_length 만큼
+// 단위시간이랑 트럭번호랑 독립적으로 시행
