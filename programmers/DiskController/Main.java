b0cfe323dbadd3ae75c069c7bde8b6c8568e7d97
Raccoon <mixkio@naver.com>
2021-11-25 22:39:04 +0900

    main

--- programmers/DiskController/Main.java
+++ programmers/DiskController/Main.java
@@ -2,7 +2,7 @@ package DiskController;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] jobs = { { 0, 10 }, { 2, 12 }, { 9, 19 }, { 15, 17 } };
+        int[][] jobs = { { 0, 3 }, { 1, 9 }, { 2, 6 } };
         Solution solution = new Solution();
         System.out.println(solution.solution(jobs));
     }
