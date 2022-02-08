372aaf0d2f7da2f6565544b2c53461d083bdb186
mixkio <mixkio@naver.com>
2022-02-08 23:35:04 +0900

    main

--- baekjoon/b14465.java
+++ baekjoon/b14465.java
@@ -12,5 +12,27 @@ public class b14465 {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = 1;
+        }
+        for (int i = 0; i < B; i++) {
+            arr[Integer.parseInt(br.readLine()) - 1] = 0;
+        }
+        int s = 0;
+        int e = K - 1;
+        int sum = 0;
+        for (int i = 0; i < K; i++) {
+            sum += arr[i];
+        }
+        int max = sum;
+        while (e < N - 1) {
+            sum -= arr[s++];
+            sum += arr[++e];
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        System.out.println(K - max);
     }
 }
---
+++ baekjoon/b20366.java
@@ -0,0 +1,51 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class b20366 {
+    static int[] arr;
+    static int[] output;
+    static boolean[] selected;
+    static int N;
+
+    static void recur(int cur) {
+        if (cur == 0) {
+
+        }
+        for (int i = 0; i < N; i++) {
+
+        }
+        recur(cur + 1);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        selected = new boolean[N];
+        output = new int[(N * (N - 1)) / 2];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        recur(0);
+        Arrays.sort(output);
+        int s = 0;
+        int e = 2;
+        int min = output[e] - output[s];
+        while (e < ((N * (N - 1)) / 2) - 1) {
+            s++;
+            e++;
+            if ((output[e] - output[s]) < min) {
+                min = (output[e] - output[s]);
+            }
+        }
+        System.out.println(min);
+    }
+}
