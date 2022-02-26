219e26e35ca8c030f43b13cf866559f42d68aaea
Raccoon <mixkio@naver.com>
2022-02-26 17:41:39 +0900

    main

---
+++ baekjoon/b3985.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b3985 {
+    static int L;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        L = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+    }
+}
