537bc59db1821c268347ca5f9a2c436d4977f822
mixkio <mixkio@naver.com>
2022-02-27 01:09:25 +0900

    main

--- baekjoon/b3985.java
+++ baekjoon/b3985.java
@@ -12,7 +12,40 @@ public class b3985 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        int expect = Integer.MIN_VALUE;
+        int expect_max = 0;
+        int real_max = 0;
         L = Integer.parseInt(br.readLine());
         N = Integer.parseInt(br.readLine());
+        int[] counter = new int[L + 1];
+        int[] max_counter = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            for (int j = start; j <= end; j++) {
+                if (counter[j] == 0) {
+                    counter[j] = i;
+                }
+            }
+            if (expect < end - start) {
+                expect = end - start;
+                expect_max = i;
+            }
+        }
+        for (int i = 1; i <= L; i++) {
+            if (counter[i] == 0)
+                continue;
+            max_counter[counter[i]]++;
+        }
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i <= N; i++) {
+            if (ans < max_counter[i]) {
+                ans = max_counter[i];
+                real_max = i;
+            }
+        }
+        System.out.println(expect_max);
+        System.out.println(real_max);
     }
 }
