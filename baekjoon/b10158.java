f9496745d8a637927f3283b98e4f457b597358ad
mixkio <mixkio@naver.com>
2022-02-28 21:28:18 +0900

    main

---
+++ baekjoon/b10158.java
@@ -0,0 +1,7 @@
+package baekjoon;
+
+public class b10158 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ baekjoon/b8320.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class b8320 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in); // 약수의 개수 ?
+    }
+}
