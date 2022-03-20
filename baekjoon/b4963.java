ca0ca9b854dfbd0f8e49b54d54dd488cfb8568b3
mixkio <mixkio@naver.com>
2022-03-20 15:45:39 +0900

    main

--- baekjoon/b4963.java
+++ baekjoon/b4963.java
@@ -7,16 +7,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Point {
-    int x;
-    int y;
-
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class b4963 {
     static int w;
     static int h;
@@ -25,21 +15,29 @@ public class b4963 {
     static int[] dy = { -1, 1, 0, 0, -1, -1, 1, 1 };
     static int[] dx = { 0, 0, -1, 1, -1, 1, -1, 1 };
 
+    static class Point {
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     static void bfs(int x, int y) {
         Queue<Point> queue = new LinkedList<>();
         queue.add(new Point(x, y));
         visited[x][y] = true;
         while (!queue.isEmpty()) {
             Point current = queue.poll();
-            if (visited[current.x][current.y])
-                continue;
             for (int i = 0; i < 8; i++) {
                 int sx = current.x + dy[i];
                 int sy = current.y + dx[i];
-                if (sx < 0 || sy < 0 || sx > h - 1 || sy > w - 1 || visited[sx][sy])
+                if (sx < 0 || sy < 0 || sx > h - 1 || sy > w - 1 || visited[sx][sy] || input[sx][sy] == 0)
                     continue;
                 queue.add(new Point(sx, sy));
-                visited[current.x][current.y] = true;
+                visited[sx][sy] = true;
             }
         }
 
