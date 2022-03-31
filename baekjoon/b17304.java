5865edc19e91969a6bf48e4dacd00c2bee9dd0f1
Raccoon <mixkio@naver.com>
2022-03-31 23:10:32 +0900

    main

--- baekjoon/b17304.java
+++ baekjoon/b17304.java
@@ -3,62 +3,46 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 public class b17304 {
     static int N;
     static int M;
-    static List[] input;
+    static int[] node;
+    static int[][] map;
     static int cnt = 0;
+    static int cnt_with = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Set<Integer> set = new TreeSet<>();
-        Map<Integer, Integer> submap = new TreeMap<>();
+        List<Integer> list = new LinkedList<Integer>();
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        input = new List[N + 1];
-        for (int i = 1; i < N + 1; i++) {
-            input[i] = new ArrayList<Integer>();
-        }
-        int totalcnt = 0;
+        node = new int[N + 1];
+        map = new int[N + 1][N + 1];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            input[from].add(to);
-            set.add(to);
-            if (input[to].contains(from)) {
-                set.remove(from);
-                set.remove(to);
-                submap.put(from, to);
-                submap.put(to, from);
+            map[from][to] = 1;
+            if (node[to] != -1)
+                node[to] = 1;
+            if (map[to][from] == 1) {
+                node[from] = -1;
+                node[to] = -1;
             }
         }
-        totalcnt += set.size();
-        while (set.size() != N) {
-            if (submap.isEmpty()) {
-                break;
+        for (int j = 1; j < N + 1; j++) {
+            if (node[j] == 0) {
+                cnt++;
             }
-            for (Integer integer : submap.keySet()) {
-                if (!set.contains(integer)) {
-                    totalcnt++;
-                    submap.remove(submap.get(integer));
-                    submap.remove(integer);
-                    break;
-                }
+            if (node[j] == -1) {
+                cnt_with++;
             }
         }
-        if (totalcnt == N)
-            System.out.println("YES");
-        else
-            System.out.println("NO");
+        cnt_with /= 2;
     }
 }
