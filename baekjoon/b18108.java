83c40fbcae82dd7703db0bd7e65db5642652efeb
Raccoon <mixkio@naver.com>
2022-04-20 16:41:18 +0900

    main

---
+++ baekjoon/b18108.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class b18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() - 543);
+    }
+}
